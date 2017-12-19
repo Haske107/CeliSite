889f04c0dcb0aff6e4db0f78277dc744304e0e7a
Jeff <Haske107@mail.chapman.edu>
2017-12-18 18:37:04 -0800

    fix file

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var fav = require('../public/favicon.ico');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 router.get('/favicon.ico', function(req, res, next) {
-    res.send('favicon.ico');
+    res.send(fav);
 });
 
 module.exports = router;
