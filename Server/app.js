fcd7479a512df116f14d2a02dee4c5c2f91b8ac1
Jeff <Haske107@mail.chapman.edu>
2017-12-18 23:12:42 -0800

    t

--- Server/app.js
+++ Server/app.js
@@ -10,6 +10,25 @@ var users = require('./routes/users');
 
 var app = express();
 
+const forceSSL = function() {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
